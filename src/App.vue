957591f7553daec878d99085a37640ee68ed7e88
风度万人迷 <13526688824@163.com>
2019-09-02 14:04:24 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+   <h1>hello</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="stylus">
+@import "~common/stylus/variable.styl";
+#app
+  color: $color-theme
+
+</style>
